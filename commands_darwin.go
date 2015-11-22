f2b1962929d9f8eeca38eff3ea22126d582b949b
Uwe Dauernheim <uwe@dauernheim.net>
2015-11-22 09:59:42 +0000

    Constrain user input values

--- commands_darwin.go
+++ commands_darwin.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os/exec"
 
 	"github.com/ian-kent/go-log/log"
@@ -9,7 +10,7 @@ import (
 
 // SetVolume takes a float value between [0,1] and sets the global output volume.
 func SetVolume(vNorm float64) {
-	v := 7 * vNorm
+	v := 7 * constrain(vNorm, 0, 1)
 	log.Debug("Setting volume to %v (%v)", vNorm, v)
 
 	cmd := []string{"/usr/bin/osascript", "-e", fmt.Sprintf("set volume %f", v)}
@@ -19,7 +20,7 @@ func SetVolume(vNorm float64) {
 
 // SetBrightness takes a float value betweem [0,1] and sets the global screen brightness.
 func SetBrightness(vNorm float64) {
-	v := int(16 * vNorm)
+	v := int(16 * constrain(vNorm, 0, 1))
 	log.Debug("Setting brightness to %v (%v)", vNorm, v)
 
 	for i := 1; i <= 16; i++ {
@@ -27,7 +28,10 @@ func SetBrightness(vNorm float64) {
 		out, err := exec.Command(cmd[0], cmd[1:]...).CombinedOutput()
 		AssertNoErr(err, fmt.Sprintf("Failed execute cmd: %v: %s", cmd, string(out)))
 	}
+}
 
+func constrain(v float64, min float64, max float64) float64 {
+	return math.Min(max, math.Max(min, v))
 	for i := 1; i <= v; i++ {
 		cmd := []string{"/usr/bin/osascript", "-e", "tell application \"System Events\" to key code 113"}
 		out, err := exec.Command(cmd[0], cmd[1:]...).CombinedOutput()
